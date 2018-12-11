aa244dbfb3ccc98d00dbcf5165dae33cbf5f172a
your name here <theo@dali.dartmouth.edu>
2018-12-11 13:49:46 -0800

    Refactor entry file to take a react component and create App/Root components

--- frontend/facebook.jsx
+++ frontend/facebook.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, signin, logout } from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const store = configureStore();
+
+  //testing only
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.signin = signin
-  window.signup = signup 
-  window.logout = logout
+  //delete these later ^
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Facebook</h1>, root)
+  ReactDOM.render(<Root store={store}/>, root)
 });
 
