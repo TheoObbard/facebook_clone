74c864f7914090958c80ac051d97845825261f8f
your name here <theo@dali.dartmouth.edu>
2018-12-11 14:30:25 -0800

    Build signup component

--- frontend/components/session/signup.js
+++ frontend/components/session/signup.js
@@ -1,10 +1,53 @@
 import React from 'react';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '', 
+      name: '', 
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleInput(field) {
+    return (e) => {
+      this.setState({
+        [field]: e.target.value
+      });
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.signup(this.state)
+      .then( () => this.props.history.push('/'));
+  }
 
   render() {
     return (
-      <h1>This is your signup component</h1>
+      <div>
+        <h1>Sign Up</h1>
+        <form>
+          <label>
+            email 
+            <input type='text' value={this.state.email} onChange={this.handleInput('email')}></input>
+          </label>
+
+          <label>
+            name
+            <input type='text' value={this.state.name} onChange={this.handleInput('name')}></input>
+          </label>
+
+          <label>
+            password
+            <input type='password' value={this.state.password} onChange={this.handleInput('password')}></input>
+          </label>
+
+          <input type='submit' onClick={this.handleSubmit}>Sign Up</input>
+        </form>
+      </div>
     );
   }
 };
