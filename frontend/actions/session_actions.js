7d4f0146f8c82fd956257138c9e78329e6dea756
your name here <theo@dali.dartmouth.edu>
2018-12-10 22:44:38 -0800

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,41 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+export const login = (user) => {
+  SessionApiUtil.signin(user).then(
+    (currentUser) => dispatch(receiveCurrentUser(currentUser))
+  )
+};
+
+export const signup = (user) => {
+  SessionApiUtil.signup(user).then(
+    (currentUser) => dispatch(receiveCurrentUser(currentUser))
+  )
+};
+
+export const logout = () => {
+  SessionApiUtil.logout().then(
+    () => dispatch(logoutCurrentUser())
+  )
+};
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER, 
+    currentUser
+  }
+};
+
+export const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  }
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    errors
+  }
+};
