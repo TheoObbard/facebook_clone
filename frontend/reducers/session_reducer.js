a88f3687bbdb8afec0b3368d811147edc054b85d
your name here <theo@dali.dartmouth.edu>
2018-12-11 11:04:56 -0800

    Session Actions reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const nullState = { id: null }
+
+const sessionReducer = (oldState = nullState, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { id: action.currentUser.id}
+    case LOGOUT_CURRENT_USER:
+      return nullState
+    default:
+      return oldState;
+  }
+};
+
+export default sessionReducer;
