9ca35b46faef56929a2779c24db80acd5ef2aa2b
your name here <theo@dali.dartmouth.edu>
2018-12-11 11:33:13 -0800

    Add session errors reducer and errors reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const login = (user) => {
   SessionApiUtil.signin(user).then(
@@ -36,6 +37,7 @@ export const logoutCurrentUser = () => {
 
 export const receiveErrors = (errors) => {
   return {
+    type: RECEIVE_SESSION_ERRORS,
     errors
   }
 };
---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const sessionErrorsReducer = (oldState = [], action) => {
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors      
+    case RECEIVE_CURRENT_USER:
+      return []
+    default:
+      return oldState;
+  }
+};
+
+export default sessionErrorsReducer;
