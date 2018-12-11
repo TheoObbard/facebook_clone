525fea9a6285a82966525fcd9ff6e8f0152cb0c3
your name here <theo@dali.dartmouth.edu>
2018-12-11 13:40:36 -0800

    Add store and fix bugs/test

--- frontend/facebook.jsx
+++ frontend/facebook.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, signin, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", ()=>{
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.signin = signin
   window.signup = signup 
   window.logout = logout
