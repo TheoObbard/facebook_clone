fe164c7de9381899f216993373b82cef800cae84
your name here <theo@dali.dartmouth.edu>
2018-12-11 11:35:42 -0800

    Add errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,1 +1,6 @@
 import { combineReducers } from 'redux';
+import sessionErrorsReducer from './session_errors_reducer';
+
+const errorsReducer = combineReducers({session: sessionErrorsReducer});
+
+export default errorsReducer;
