b3fb35e54f83abd861519a99fdd6b89c6d37e8e6
your name here <theo@dali.dartmouth.edu>
2018-12-17 10:56:39 -0800

    Add styling for comments and adding comments

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -12,7 +12,7 @@ class CommentItem extends React.Component {
         <div className='comment_container'>
           <img className='commenter_img' src={this.props.poster.profilePicUrl} alt=""/>
           <div className='comment_username_and_body'>
-            <Link className='post_item_commenter_name' to={`/user/${this.props.poster.id}`}><p>{this.props.poster.name}</p></Link>
+            <Link className='post_item_commenter_name' to={`/user/${this.props.poster.id}`}><p className='commenter_name_text'>{this.props.poster.name}</p></Link>
             <div className='comment_body'>
               <p>{this.props.comment.body}</p>
             </div>
