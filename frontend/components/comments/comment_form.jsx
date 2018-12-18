ef57aeb482ba688bc8e4ecc2618376bdc0587e51
your name here <theo@dali.dartmouth.edu>
2018-12-17 21:04:06 -0800

    fix small bug with commenter image being the wrong person

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -38,7 +38,7 @@ class CommentForm extends React.Component {
   render() {
     return (
       <div className='comment_form_container'>
-        <img className='commenter_img' src={this.props.user.profilePicUrl} alt=""/>
+        <img className='commenter_img' src={this.props.currentUser.profilePicUrl} alt=""/>
         <form className='comment_form' onSubmit={this.handleSubmit}>
           <label className='comment_input'>
             <input className='comment_object' onChange={this.handleType()} placeholder={`Write a comment...`} value={this.state.body} type="text" name="" id=""/>
