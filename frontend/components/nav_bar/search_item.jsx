b80ce6a9129a351e8a346609065d6b1003bc0e4d
your name here <theo@dali.dartmouth.edu>
2018-12-19 11:13:25 -0800

    Style search bar dropdown and add functionality to click to a users page

--- frontend/components/nav_bar/search_item.jsx
+++ frontend/components/nav_bar/search_item.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchItem extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  redirect() {
+
+  }
+
   render() {
     return (
-      <li>
-        <div>
-          {this.props.user.name}
-        </div>
-      </li>
+      <Link className='search_item' to={`/user/${this.props.user.id}`}>
+        <div className='search_user_name'>{this.props.user.name}</div>
+      </Link>
     )
   }
 };
