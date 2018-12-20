3f85a5805840df2c9e20ce9976c7d6d2274d8e47
your name here <theo@dali.dartmouth.edu>
2018-12-20 12:04:42 -0800

    Add styling to like button

--- frontend/components/likes/likes.jsx
+++ frontend/components/likes/likes.jsx
@@ -12,17 +12,49 @@ class Likes extends React.Component {
       if (checking.user_id === this.props.currentUser.id && 
           checking.object_type === this.props.object_type && 
           checking.object_id === this.props.object_id) {
-        this.props.removeLike(this.props.currentUser.id, checking.id)
-        return
+        return (
+          <div className='comment_post_container'>
+            <div onClick={() => this.props.removeLike(this.props.currentUser.id, checking.id)}>
+              <div className='post_like_button_clicked'>
+                <i class="far fa-thumbs-up"></i><span className='like_post_button_text'>Like</span>
+              </div>
+            </div>
+          </div>
+        )
       }
     }
-    this.props.addLike(this.props.currentUser.id, this.props.object_type, this.props.object_id)
+    return (
+      <div className='comment_post_container'>
+        <div onClick={() => this.props.addLike(this.props.currentUser.id, this.props.object_type, this.props.object_id)}>
+          <div className='post_like_button'>
+            <i class="far fa-thumbs-up"></i><span className='like_post_button_text'>Like</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  handleLikeButtonType() {
+    if (this.props.object_type === 'Post') {
+      return (
+        <div>
+          {this.handleLikeLogic()}
+        </div>
+      )
+    } else {
+      return (
+        <div onClick={() => this.handleLikeLogic()}>
+          {/* styling for comment button */}
+          Like Comment
+        </div>
+      )
+    }
   }
 
   render() {
     return (
-      <div onClick={() => this.handleLikeLogic()}>
-        Like
+      <div>
+        {this.handleLikeButtonType()}
       </div>
     )
   }
